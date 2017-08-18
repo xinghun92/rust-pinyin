290cdaad3c5db109ae2fae4fd77934f6972e21e2
wangsijie <wangsijie@bytedance.com>
2017-08-18 18:58:50 +0800

    feat: 去掉拼音库的声调，支持多音字
    Change-Id: I66173ceefcc779cbbb1fc70e3766077310267efc

--- build.rs
+++ build.rs
@@ -19,40 +19,6 @@ fn main() {
     write!(&mut file, "static PINYIN_MAP: ::phf::Map<u32, &'static str> = ").unwrap();
     builder.build(&mut file).unwrap();
     write!(&mut file, ";\n").unwrap();
-
-    // 带声调字符
-    write!(&mut file, "static PHONETIC_SYMBOL_MAP: ::phf::Map<&'static str, &'static str> = ").unwrap();
-    phf_codegen::Map::new()
-        .entry("ā", "\"a1\"")
-        .entry("á", "\"a2\"")
-        .entry("ǎ", "\"a3\"")
-        .entry("à", "\"a4\"")
-        .entry("ē", "\"e1\"")
-        .entry("é", "\"e2\"")
-        .entry("ě", "\"e3\"")
-        .entry("è", "\"e4\"")
-        .entry("ō", "\"o1\"")
-        .entry("ó", "\"o2\"")
-        .entry("ǒ", "\"o3\"")
-        .entry("ò", "\"o4\"")
-        .entry("ī", "\"i1\"")
-        .entry("í", "\"i2\"")
-        .entry("ǐ", "\"i3\"")
-        .entry("ì", "\"i4\"")
-        .entry("ū", "\"u1\"")
-        .entry("ú", "\"u2\"")
-        .entry("ǔ", "\"u3\"")
-        .entry("ù", "\"u4\"")
-        .entry("ü", "\"v0\"")
-        .entry("ǘ", "\"v2\"")
-        .entry("ǚ", "\"v3\"")
-        .entry("ǜ", "\"v4\"")
-        .entry("ń", "\"n2\"")
-        .entry("ň", "\"n3\"")
-        .entry("", "\"m2\"")
-        .build(&mut file)
-        .unwrap();
-    write!(&mut file, ";\n").unwrap();
 }
 
 fn escape_str(s: &str) -> String {
